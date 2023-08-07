70e26caa7e12d9963377c588a38863d34ecbea3b
XEM <itstaureans@gmail.com>
2023-08-07 15:47:39 +0700

    style (auth): Change clerk components style

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Dazh | Sign In",
+    description: "Sign In using Clerk"
+}
 
 export default function Page() {
     return (
         <div className="flex justify-center items-center bg-foreground mx-auto py-auto min-h-screen">
-            <SignIn />;
+            <SignIn appearance={{
+                elements: {
+                    socialButtonsBlockButton: 'bg-primary text-secondary hover:text-primary transition duration-300',
+                    formButtonPrimary: 'bg-primary hover:bg-muted-foreground text-sm normal-case transition duration-300',
+                    formFieldInput: 'rounded-lg ring-1 focus:ring-primary focus:border-primary transition duration-300'
+                }
+            }} />;
         </div>
     )
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,21 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Dazh | Sign Up",
+    description: "Sign Up using Clerk"
+}
 
 export default function Page() {
     return (
         <div className="flex justify-center items-center bg-foreground mx-auto py-auto min-h-screen">
-            <SignUp />;
+            <SignUp appearance={{
+                elements: {
+                    socialButtonsBlockButton: 'bg-primary text-secondary hover:text-primary transition duration-300',
+                    formButtonPrimary: 'bg-primary hover:bg-muted-foreground text-sm normal-case transition duration-300',
+                    formFieldInput: 'rounded-lg ring-1 focus:ring-primary focus:border-primary transition duration-300'
+                }
+            }} />;
         </div>
     )
 }
