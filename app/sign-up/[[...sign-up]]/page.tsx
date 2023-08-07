7a66792d8212e94ba82e50a53b82fb4dbe6f6813
XEM <itstaureans@gmail.com>
2023-08-07 14:04:46 +0700

    feat (Auth): Added sign-in & sign-up page with login in navbar

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+    return (
+        <div className="flex justify-center items-center bg-foreground mx-auto py-auto min-h-screen">
+            <SignUp />;
+        </div>
+    )
+}
