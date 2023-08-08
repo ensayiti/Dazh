3aee63bc97e402d7d509e3b15ff8f7b451e0844c
XEM <itstaureans@gmail.com>
2023-08-08 12:56:49 +0700

    Fix (Dashboard): Context

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -42,13 +42,13 @@ export default function Dashboard() {
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth='2' className="h-4 w-4 text-muted-foreground"><path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" /></svg>
                                 </CardHeader>
                                 <CardContent>
-                                    <div className="text-2xl font-bold">IDR 231.130</div>
+                                    <div className="text-2xl font-bold">IDR 52.965.142</div>
                                     <p className="text-xs text-muted-foreground">+20.1% from last month</p>
                                 </CardContent>
                             </Card>
                             <Card>
                                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+                                    <CardTitle className="text-sm font-medium">Registered Users</CardTitle>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth='2' className="h-4 w-4 text-muted-foreground">
                                         <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
                                         <circle cx="9" cy="7" r="4" />
@@ -56,32 +56,32 @@ export default function Dashboard() {
                                     </svg>
                                 </CardHeader>
                                 <CardContent>
-                                    <div className="text-2xl font-bold">+2350</div>
+                                    <div className="text-2xl font-bold">87</div>
                                     <p className="text-xs text-muted-foreground">+180.1% from last month</p>
                                 </CardContent>
                             </Card>
                             <Card>
                                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                                    <CardTitle className="text-sm font-medium">Total Sales</CardTitle>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth='2' className="h-4 w-4 text-muted-foreground">
                                         <rect width="20" height="14" x="2" y="5" rx="2" />
                                         <path d="M2 10h20" />
                                     </svg>
                                 </CardHeader>
                                 <CardContent>
-                                    <div className="text-2xl font-bold">+12,352</div>
+                                    <div className="text-2xl font-bold">1125</div>
                                     <p className="text-xs text-muted-foreground">+19% from last month</p>
                                 </CardContent>
                             </Card>
                             <Card>
                                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Active Now</CardTitle>
+                                    <CardTitle className="text-sm font-medium">Active Products</CardTitle>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth='2' className="h-4 w-4 text-muted-foreground">
                                         <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
                                     </svg>
                                 </CardHeader>
                                 <CardContent>
-                                    <div className="text-2xl font-bold">+574</div>
+                                    <div className="text-2xl font-bold">455</div>
                                     <p className="text-xs text-muted-foreground">+201 since last hour</p>
                                 </CardContent>
                             </Card>
--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -6,24 +6,35 @@ export function RecentSales() {
             <div className='flex items-center'>
                 <Avatar className='h-9 w-9'>
                     <AvatarImage src='/avatar/01.png' alt='Avatar' />
-                    <AvatarFallback>OM</AvatarFallback>
+                    <AvatarFallback>PA</AvatarFallback>
                 </Avatar>
                 <div className='ml-4 space-y-1'>
-                    <p className='text-sm font-medium leading-none'>Olivia Martin</p>
-                    <p className='text-sm text-muted-foreground'>olivia.martin@gmail.com</p>
+                    <p className='text-sm font-medium leading-none'>Putri Ambarsari</p>
+                    <p className='text-sm text-muted-foreground'>ptr.ambasari13@gmail.com</p>
                 </div>
-                <div className='ml-auto font-medium'>+$1.999</div>
+                <div className='ml-auto font-medium'>IDR 512.000</div>
             </div>
             <div className='flex items-center'>
                 <Avatar className='h-9 w-9'>
                     <AvatarImage src='/avatar/01.png' alt='Avatar' />
-                    <AvatarFallback>JL</AvatarFallback>
+                    <AvatarFallback>HA</AvatarFallback>
                 </Avatar>
                 <div className='ml-4 space-y-1'>
-                    <p className='text-sm font-medium leading-none'>Jackson Lee</p>
-                    <p className='text-sm text-muted-foreground'>jackson.lee@gmail.com</p>
+                    <p className='text-sm font-medium leading-none'>Hilal Agus</p>
+                    <p className='text-sm text-muted-foreground'>hilal.ha@gmail.com</p>
                 </div>
-                <div className='ml-auto font-medium'>+$5,999</div>
+                <div className='ml-auto font-medium'>IDR 1.900.000</div>
+            </div>
+            <div className='flex items-center'>
+                <Avatar className='h-9 w-9'>
+                    <AvatarImage src='/avatar/01.png' alt='Avatar' />
+                    <AvatarFallback>AS</AvatarFallback>
+                </Avatar>
+                <div className='ml-4 space-y-1'>
+                    <p className='text-sm font-medium leading-none'>Ahmad Samudra</p>
+                    <p className='text-sm text-muted-foreground'>asbx.official@gmail.com</p>
+                </div>
+                <div className='ml-auto font-medium'>IDR 870.000</div>
             </div>
         </div>
     )
