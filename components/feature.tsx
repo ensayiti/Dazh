609f0f86ca2456bf6b205655032a4fa2a6e5f4cd
XEM <itstaureans@gmail.com>
2023-08-09 13:13:31 +0700

    Fix (Feature): Eslint

--- components/feature.tsx
+++ components/feature.tsx
@@ -10,17 +10,17 @@ export function Feature() {
                 <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
                     <div className="bg-primary rounded-lg p-3">
                         <h3 className="flex justify-center items-center text-secondary mb-1 text-xl font-bold">Customizable <span className="inline-flex w-5 h-5 ml-2 justify-center items-center align-middle"><PencilRuler /></span></h3>
-                        <p className="text-secondary tracking-tighter leading-tight">Dazh is highly customizable, and customized according to your preferences and tastes. By using CSS Variables, you won't be confused about choosing a color palette or theme.</p>
+                        <p className="text-secondary tracking-tighter leading-tight">Dazh is highly customizable, and customized according to your preferences and tastes. By using CSS Variables, you wont be confused about choosing a color palette or theme.</p>
                     </div>
 
                     <div className="bg-primary rounded-lg p-3">
                         <h3 className="flex justify-center items-center text-secondary mb-1 text-xl font-bold">Reusable Components <span className="inline-flex w-5 h-5 ml-2 justify-center items-center align-middle"><Component /></span></h3>
-                        <p className="text-secondary tracking-tighter leading-tight">Using reusable components is one way out for time efficiency. Your time will not be wasted designing a component, section, or block. Very easy right? That's Dazh's goal!</p>
+                        <p className="text-secondary tracking-tighter leading-tight">Using reusable components is one way out for time efficiency. Your time will not be wasted designing a component, section, or block. Very easy right? Thats Dazh goal!</p>
                     </div>
 
                     <div className="bg-primary rounded-lg p-3">
                         <h3 className="flex justify-center items-center text-secondary mb-1 text-xl font-bold">All-in-One <span className="inline-flex w-5 h-5 ml-2 justify-center items-center align-middle"><CheckCircle2 /></span></h3>
-                        <p className="text-secondary tracking-tighter leading-tight">You don't need to create landing pages and dashboards in different projects. Dazh was built to meet those needs. So that your time will not run out to build two different applications.</p>
+                        <p className="text-secondary tracking-tighter leading-tight">You dont need to create landing pages and dashboards in different projects. Dazh was built to meet those needs. So that your time will not run out to build two different applications.</p>
                     </div>
                 </div>
             </div>
